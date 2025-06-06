ee81e44b586aa55c06a2f6b72df2e1c3e8266a02
Himanshu <hkkabaddi63@gmail.com>
2025-06-06 13:45:15 +0530

    update in contacus

--- src/components/Student/ContactUs.tsx
+++ src/components/Student/ContactUs.tsx
@@ -1,9 +1,7 @@
-import { Accordion, Autocomplete, Center, Container, SegmentedControl, Title } from "@mantine/core"
+import { Accordion, Autocomplete, Container, SegmentedControl, Title } from "@mantine/core"
 import { IconSearch } from "@tabler/icons-react";
 import { useState } from "react";
 
-const placeholder =
-  'It can’t help but hear a pin drop from over half a mile away, so it lives deep in the mountains where there aren’t many people or Pokémon.It was born from sludge on the ocean floor. In a sterile environment, the germs within its body can’t multiply, and it dies.It has no eyeballs, so it can’t see. It checks its surroundings via the ultrasonic waves it emits from its mouth.';
 
 const faq = [
   {
