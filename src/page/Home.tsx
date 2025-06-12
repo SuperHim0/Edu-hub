27837dba26bb0d696b1c4ba50566328c0b5ebe83
Himanshu <hkkabaddi63@gmail.com>
2025-06-12 16:05:44 +0530

    added marquee

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,4 +1,4 @@
-
+import Marquee from "react-fast-marquee";
 import Review from '../components/Courses/Review'
 import FooterSite from '../components/Home/FooterSite'
 import Header from '../components/Home/Header'
@@ -10,7 +10,9 @@ const Home = () => {
   return (
     <div className=' '>
       
-      
+        <Marquee speed={50} gradient={false} className="bg-gray-100 h-10 flex items-center justify-center">
+       ⚠️ This is an announcement: The server is hosted on Render, so it may take up to 60 seconds to respond. Please be patient.⚠️ Thank you For Visiting the site 🌟 
+        </Marquee>
         <Header/>
         <Main />
         <Review />
